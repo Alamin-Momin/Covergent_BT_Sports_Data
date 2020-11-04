d32ad008a496013a9b88e17d1b8ff01ff7269c8f
Sweta G <swetaghose16@gmail.com>
2020-11-03 20:21:02 -0600

    home page

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+class Home extends React.Component {
+    
+    render() {
+        return (
+            <div class="top-bar">
+                 <div class="logo-pic"></div>
+            </div>
+        )
+    }
+}
+
+export default Home;

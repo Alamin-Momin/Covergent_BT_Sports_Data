90e88433c0292a045d0067e05e9c5384f7881170
Sweta G <swetaghose16@gmail.com>
2020-11-17 20:11:31 -0600

    game boxes fixed

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,13 +2,21 @@ import React from 'react'
 import './Home.css'
 
 class Home extends React.Component {
-    
+
     render() {
         return (
             <div class="top-bar">
-                 <div class="logo-pic"></div>
-                 <div class = "week-box"> 
+                <div class="logo-pic"></div>
+                <div class="week-box">
                     <text> &larr; Week 15 &rarr;</text>
+                </div>
+                <div class="grid-container">
+                    <div class="game-box"><text> game 15</text> </div>
+                    <div class="game-box"> <text> game 15</text></div> 
+                    <div class="game-box"> <text> game 15</text></div> 
+                    <div class="game-box"> <text> game 15</text></div> 
+                    <div class="game-box"> <text> game 15</text></div> 
+                    <div class="game-box"> <text> game 15</text></div>
                 </div>
             </div>
 

f4a08442c8833f9051ad27ea0c327842879c5f4a
Sweta G <swetaghose16@gmail.com>
2020-11-29 15:21:34 -0600

    LOGOS

--- src/component/Home.js
+++ src/component/Home.js
@@ -11,12 +11,30 @@ class Home extends React.Component {
                     <text> &larr; Week 15 &rarr;</text>
                 </div>
                 <div class="grid-container">
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
-                    <div class="game-box"><text> 35 - 17</text><div class="inside"> HOU BAL</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/baltimore-ravens.png'} alt=''/>
+                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
                 </div>
             </div>
 

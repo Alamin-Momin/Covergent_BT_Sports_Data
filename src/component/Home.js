749802da6469c5d6a2fa49e7675080df1d271349
Sweta G <swetaghose16@gmail.com>
2020-12-04 12:24:16 -0600

    logos

--- src/component/Home.js
+++ src/component/Home.js
@@ -12,32 +12,32 @@ class Home extends React.Component {
                     <text> &larr; Week 15 &rarr;</text>
                 </div>
                 <div class="grid-container">
-                    <Link to='/details'>
-                    <div class="game-box"><text> 35 - 17</text>
+                    <Link to='/dashboard'>
+                    <div class="game-box"><text> 54 - 51</text>
                         <div class="inside"><div class="column">
-                        <img src={'../images/baltimore-ravens.png'} alt=''/>
-                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL SEA</div>    </div>
+                        <img src={'../images/lar.png'} alt=''/>
+                        <img src={'../images/chiefs.png'} alt=''/></div>LAR KAN</div>    </div>
                     </Link> 
-                    <div class="game-box"><text> 35 - 17</text>
+                    <div class="game-box"><text> 7 - 48</text>
                     <div class="inside"><div class="column">
-                        <img src={'../images/dallas-cowboys.png'} alt=''/>
-                        <img src={'../images/chiefs.png'} alt=''/></div>DAL KAN</div> </div>
-                    <div class="game-box"><text> 35 - 17</text>
+                        <img src={'../images/eagles.png'} alt=''/>
+                        <img src={'../images/saint.png'} alt=''/></div>PHI NO</div> </div>
+                    <div class="game-box"><text> 25 - 20</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/viking.png'} alt=''/>
+                        <img src={'../images/bears.png'} alt=''/></div>MIN CHI</div> </div>
+                    <div class="game-box"><text> 23 - 22</text>
+                    <div class="inside"><div class="column">
+                        <img src={'../images/broncos.png'} alt=''/>
+                        <img src={'../images/lac.png'} alt=''/></div>DEN LAC</div> </div>
+                    <div class="game-box"><text> 23 - 21</text>
                     <div class="inside"><div class="column">
                         <img src={'../images/texans.png'} alt=''/>
-                        <img src={'../images/nep.png'} alt=''/></div>HOU NEP</div> </div>
-                    <div class="game-box"><text> 35 - 17</text>
+                        <img src={'../images/washington.png'} alt=''/></div>HOU WAS</div> </div>
+                    <div class="game-box"><text> 10 - 38</text>
                     <div class="inside"><div class="column">
-                        <img src={'../images/baltimore-ravens.png'} alt=''/>
-                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
-                    <div class="game-box"><text> 35 - 17</text>
-                    <div class="inside"><div class="column">
-                        <img src={'../images/baltimore-ravens.png'} alt=''/>
-                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
-                    <div class="game-box"><text> 35 - 17</text>
-                    <div class="inside"><div class="column">
-                        <img src={'../images/baltimore-ravens.png'} alt=''/>
-                        <img src={'../images/seattle-seahawks.png'} alt=''/></div>BAL HOU</div> </div>
+                        <img src={'../images/titans.png'} alt=''/>
+                        <img src={'../images/colts.png'} alt=''/></div>TEN IND</div> </div>
                 </div>
             </div>
 
